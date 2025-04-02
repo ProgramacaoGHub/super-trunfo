65d810ee71482862aa1c6341b87b40d35ef96f59
ProgramacaoGHub <kaueandradre416@gmail.com>
2025-04-02 00:20:42 +0000

    jogo Finalizado

--- SUPER_TRUNFO.c
+++ SUPER_TRUNFO.c
@@ -14,33 +14,77 @@ int main() {
     int pontosTuristicos1 = 10;
 
     //SAIDA DOS DADOS DA CARTA 1:
+    printf (" Digite uma letra de 'A' a 'H' Para o Estado:\n" );
+    scanf ("%c", &estado1);
     printf ("Estado: %c\n", estado1);
+
+    printf ("Digite um codigo Código com 3 caracteres:\n");
+    scanf ("%s", &nome1);
     printf ("Código: %s\n", nome1);
-    printf ("Nome da cidade: %s\n", cidade1);
+
+    printf ("Digite o Nome da cidade:\n");
+    scanf("%s", &cidade1);
+    printf ("Cidade: %s\n", cidade1);
+
+    printf ("Digite o numero de Habitentes:\n");
+    scanf ("%f", &populacao1);
     printf ("População: %f\n", populacao1);
-    printf ("Área: %f km²\n",area1);
-    printf ("PIB: %f milhões de reais\n",PIB1);
-    printf ("Número de Pontos Turísticos: %i\n",pontosTuristicos1);
-    printf ("\n");
- 
-    //INFORMAÇÕES DA CARTA 2:
-     printf ("CARTA 2\n");
-     char estado2 = 'B';
-     char nome2[5]= "A02";
-     char cidade2 [50] = "Batatais";
-     float populacao2 = 1000;
-     float area2 = 850.718;
-     float PIB2 =  2.42;
-     int pontosTuristicos2 = 15;
- 
-     //SAIDA DOS DADOS DA CARTA 2:
-     printf ("Estado: %c\n", estado2);
-     printf ("Código: %s\n", nome2);
-     printf ("Nome da cidade: %s\n", cidade2);
-     printf ("População: %f\n", populacao2);
-     printf ("Área: %f km²\n",area2);
-     printf ("PIB: %f Bilhões de reais\n",PIB2);
-     printf ("Número de Pontos Turísticos: %i\n",pontosTuristicos2);
+
+    printf ("Digite a Área da Cidade:\n");
+    scanf ("%f", &area1);
+    printf ("Área: %f km²\n", area1);
+
+
+    printf ("Digite o PIB da Cidade:\n");
+    scanf ("%f", &populacao1);
+    printf ("PIB: %f reais\n", populacao1);
+
+
+   printf ("Digite o Número de Pontos Turísticos:\n");
+   scanf ("%i", &pontosTuristicos1);
+   printf ("Pontos Turísticos: %i\n", pontosTuristicos1);
+   printf ("\n");
     
+   printf ("CARTA 2\n");
+   char estado2 = 'A';
+   char nome2[3]= "A00";
+   char cidade2 [50] = "Anhanguera";
+   float populacao2 = 1000;
+   float area2 = 33.3;
+   float PIB2 = 21.7;
+   int pontosTuristicos2 = 10;
+
+   //SAIDA DOS DADOS DA CARTA 2:
+   printf (" Digite uma letra de 'A' a 'H' Para o Estado:\n" );
+   scanf ("%c", &estado2);
+   printf ("Estado: %c\n", estado2);
+
+   printf ("Digite um codigo Código com 3 caracteres:\n");
+   scanf("%s", &nome2);
+   printf ("Código: %s\n", nome2);
+
+   printf ("Digite o Nome da cidade:\n");
+   scanf("%s", &cidade2);
+   printf ("Cidade: %s\n", cidade2);
+
+   printf ("Digite o numero de Habitentes:\n");
+   scanf ("%f", &populacao2);
+   printf ("População: %f\n", populacao2);
+
+   printf ("Digite a Área da Cidade:\n");
+   scanf ("%f", &area2);
+   printf ("Área: %f km²\n", area2);
+
+
+   printf ("Digite o PIB da Cidade:\n");
+   scanf ("%f", &populacao2);
+   printf ("PIB: %f reais\n", populacao2);
+
+
+  printf ("Digite o Número de Pontos Turísticos:\n");
+  scanf ("%i", &pontosTuristicos2);
+  printf ("Pontos Turísticos: %i\n", pontosTuristicos2);
+
+
     return 0;
 }
